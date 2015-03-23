bfaa61f07fcfcb8d0da4eda0f482f99d83b142e6
theaddy <46258197+bakedroll@users.noreply.github.com>
2015-03-23 16:39:38 +0000

    - earth shader

--- OnePercent/GameApplication.cpp
+++ OnePercent/GameApplication.cpp
@@ -26,41 +26,41 @@ int GameApplication::run()
 	group->getStateSet()->setMode(GL_DEPTH_TEST, StateAttribute::ON);
 	group->getStateSet()->setMode(GL_LIGHTING, StateAttribute::ON);
 
-	group->getStateSet()->setMode(GL_LIGHT0 , StateAttribute::OFF);
-	group->getStateSet()->setMode(GL_LIGHT1, StateAttribute::ON);
+	group->getStateSet()->setMode(GL_LIGHT0 , StateAttribute::ON);
+	//group->getStateSet()->setMode(GL_LIGHT1, StateAttribute::ON);
 
 	ref_ptr<LightModel> lightmodel = new LightModel();
-	lightmodel->setAmbientIntensity(Vec4(1.0f, 1.0f, 1.0f, 1.0f));
+	lightmodel->setAmbientIntensity(Vec4(0.0f, 0.0f, 0.0f, 1.0f));
 	group->getStateSet()->setAttributeAndModes(lightmodel, StateAttribute::ON);
 
 	ref_ptr<Light> light1 = new Light();
 	ref_ptr<LightSource> ls1 = new LightSource();
 
-	ref_ptr<Light> light2 = new Light();
-	ref_ptr<LightSource> ls2 = new LightSource();
+	//ref_ptr<Light> light2 = new Light();
+	//ref_ptr<LightSource> ls2 = new LightSource();
 
-	light1->setLightNum(0);
+	/*light1->setLightNum(0);
 	light1->setDiffuse(Vec4(1.0, 0.7, 0.3, 1.0));
 	light1->setSpecular(Vec4(1.0, 1.0, 1.0, 1.0));
 	light1->setAmbient(Vec4(0.0, 0.0, 0.0, 1.0));
 	light1->setPosition(Vec4(0.0, 0.0, 0.0, 1.0));
 
+	ls1->setLight(light1);*/
+
+	light1->setLightNum(0);
+	light1->setDiffuse(Vec4(1.0, 1.0, 1.0, 1.0));
+	light1->setSpecular(Vec4(1.0, 1.0, 1.0, 1.0));
+	light1->setAmbient(Vec4(0.0, 0.0, 0.0, 1.0));
+	light1->setPosition(Vec4(-1.0, -0.5, 0.0, 0.0));
+
 	ls1->setLight(light1);
 
-	light2->setLightNum(1);
-	light2->setDiffuse(Vec4(1.0, 1.0, 1.0, 1.0));
-	light2->setSpecular(Vec4(1.0, 1.0, 1.0, 1.0));
-	light2->setAmbient(Vec4(0.0, 0.0, 0.0, 1.0));
-	light2->setPosition(Vec4(-1.0, -0.5, 0.0, 0.0));
-
-	ls2->setLight(light2);
-
-	ref_ptr<PositionAttitudeTransform> transform = new PositionAttitudeTransform();
+	/*ref_ptr<PositionAttitudeTransform> transform = new PositionAttitudeTransform();
 	transform->setPosition(Vec3(5, 3, 0));
 	transform->addChild(ls1);
 
-	//group->addChild(transform);
-	group->addChild(ls2);
+	//group->addChild(transform);*/
+	group->addChild(ls1);
 
 	viewer.setSceneData(group);
 
--- OnePercent/shader/globe.frag
+++ OnePercent/shader/globe.frag
@@ -1,15 +1,21 @@
 varying vec3 N;
 varying vec3 v;
 
+uniform sampler2D colormap;
+uniform sampler2D specmap;
+uniform sampler2D nightmap;
+uniform sampler2D citymap;
+
 #define MAX_LIGHTS 1
 
 void main (void) 
 { 
-   vec4 finalColor = vec4(0.0, 0.0, 0.0, 0.0);
+   vec4 finalColor = vec4(0.0, 0.0, 0.0, 1.0);
    
    for (int i=0;i<MAX_LIGHTS;i++)
    {
       vec3 L;
+	  float ndotl, edge;
 	  
 	  if (gl_LightSource[i].position.w == 1.0)
 	  {
@@ -23,20 +29,30 @@ void main (void)
       vec3 E = normalize(-v); // we are in Eye Coordinates, so EyePos is (0,0,0) 
       vec3 R = normalize(-reflect(L,N)); 
    
+
+
       //calculate Ambient Term: 
-      vec4 Iamb = gl_FrontLightProduct[i].ambient; 
-      //calculate Diffuse Term: 
-      vec4 Idiff = gl_FrontLightProduct[i].diffuse * max(dot(N,L), 0.0);
-      Idiff = clamp(Idiff, 0.0, 1.0); 
+      vec4 Iamb = gl_FrontLightProduct[i].ambient;
+
+      //calculate Diffuse Term:
+	  ndotl = dot(N,L);
+
+      //vec4 Idiff = gl_FrontLightProduct[i].diffuse * max(ndotl, 0.0);
+      //Idiff = clamp(Idiff, 0.0, 1.0);
    
+	  ndotl = clamp(ndotl, -1.0, 1.0);
+	  edge = (clamp(ndotl * 3.0, -1.0, 1.0) + 1.0) * 0.5;
+
+      vec4 Idiff = texture2D(colormap, gl_TexCoord[0].st) * edge + (texture2D(nightmap, gl_TexCoord[0].st) + texture2D(citymap, gl_TexCoord[0].st)) * (1.0 - edge);
+
       // calculate Specular Term:
       vec4 Ispec = gl_FrontLightProduct[i].specular 
-             * pow(max(dot(R,E),0.0),0.3*gl_FrontMaterial.shininess);
+             * pow(max(dot(R,E),0.0),0.3*gl_FrontMaterial.shininess) * texture2D(specmap, gl_TexCoord[0].st);
       Ispec = clamp(Ispec, 0.0, 1.0); 
-   
-      finalColor += Iamb + Idiff + Ispec;
+
+	  finalColor += Iamb + Idiff + Ispec;
    }
    
    // write Total Color: 
-   gl_FragColor = gl_FrontLightModelProduct.sceneColor + finalColor + gl_FrontMaterial.emission; 
+   gl_FragColor = gl_FrontLightModelProduct.sceneColor + finalColor + gl_FrontMaterial.emission;
 }
