e3507b4d48beaea905152670db8049c13528e260
theaddy <theaddy@4c8e4b2f-98be-4288-a789-18abe27bfa1d>
2016-01-17 22:40:01 +0000

    - fixed poly writer

--- trunk/ImageHelperCpp/io.cpp
+++ trunk/ImageHelperCpp/io.cpp
@@ -6,21 +6,31 @@ namespace helper
 {
   void writePolyFile(ResultPointMap& points, ResultEdgeValueList& edges, char* filename)
   {
+    typedef std::map<int, int> IdMap;
+
+    IdMap ids;
+
     std::ofstream fs(filename);
     if (!fs.is_open())
       return;
 
     fs << int(points.size()) << " 2 0 0" << std::endl;
 
+    int i = 1;
     for (ResultPointMap::iterator it = points.begin(); it != points.end(); ++it)
-      fs << it->first << " " << it->second.x << " " << it->second.y << std::endl;
+    {
+      ids.insert(IdMap::value_type(it->first, i));
+
+      fs << i << " " << it->second.x << " " << it->second.y << std::endl;
+      i++;
+    }
 
     fs << int(edges.size()) << " 1" << std::endl;
 
-    int i = 1;
+    i = 1;
     for (ResultEdgeValueList::iterator it = edges.begin(); it != edges.end(); ++it)
     {
-      fs << i << " " << it->first.first << " " << it->first.second << " 1" << std::endl;
+      fs << i << " " << ids[it->first.first] << " " << ids[it->first.second] << " 1" << std::endl;
       i++;
     }
 
