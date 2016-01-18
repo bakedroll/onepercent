7689de906475cc1a83f88f71c82ab10214f68075
theaddy <46258197+bakedroll@users.noreply.github.com>
2016-01-18 18:40:39 +0000

    - more refactoring

--- ImageHelperCpp/main.cpp
+++ ImageHelperCpp/main.cpp
@@ -1,45 +1,70 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 #include <iostream>
 
 #include "detect.h"
-#include "draw.h"
 #include "reduce.h"
 #include "io.h"
+#include "draw.h"
 
-using namespace cv;
-using namespace std;
-
-int main(int argc, char** argv)
+int detectLines(int argc, char** argv)
 {
   if (argc < 4)
   {
-    cout << "Error: Not enough parameters." << endl;
+    std::cout << "Error: Not enough parameters." << std::endl;
     return -1;
   }
 
-  int result = 0;
+  const char* inputImage = argv[2];
+  float displayScale = float(atof(argv[4]));
+  int depth = atoi(argv[3]);
+  const char* outputPoly = argv[5];
+
+  cv::Mat image;
+
+  image = cv::imread(inputImage, cv::IMREAD_GRAYSCALE);
+
+  if (!image.data)
+  {
+    printf("Error: Image could not be loaded: %s\n", inputImage);
+    return -1;
+  }
+
+  cv::Mat displayImage(image.rows, image.cols, CV_8UC3);
+  cv::Mat resultImage(int(image.rows * displayScale), int(image.cols * displayScale), CV_8UC3);
+
+  cvtColor(image, displayImage, CV_GRAY2RGB);
+
+  helper::Graph graph;
+  helper::detectLines(image, displayImage, depth, graph);
+  helper::reducePoints(graph);
+  helper::writePolyFile(graph, outputPoly);
+  helper::drawGraph(resultImage, graph, displayScale);
+
+  imshow("Lines", displayImage);
+  imshow("Result", resultImage);
+  imwrite("img.png", displayImage);
+  imwrite("result.png", resultImage);
+
+  return 0;
+}
+
+int main(int argc, char** argv)
+{
+  int result;
   int mode = atoi(argv[1]);
   
   if (mode == 0)
   {
-    const char* inputImage = argv[2];
-    float displayScale = float(atof(argv[4]));
-    int depth = atoi(argv[3]);
-    const char* outputPoly = argv[5];
-
-    helper::Graph graph;
-    helper::detectLines(inputImage, displayScale, depth, graph);
-    helper::reducePoints(graph);
-    helper::writePolyFile(graph, outputPoly);
-
+    result = detectLines(argc, argv);
   }
   else
   {
-    cout << "Error: Wrong mode." << endl;
+    std::cout << "Error: Wrong mode." << std::endl;
     result = -1;
   }
 
-  waitKey(0);
+  cv::waitKey(0);
   return result;
 }
