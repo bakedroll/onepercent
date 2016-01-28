21ab5e2403e01d911eef9b48c420667701e0d3ee
theaddy <theaddy@4c8e4b2f-98be-4288-a789-18abe27bfa1d>
2016-01-28 18:43:11 +0000

    - updated boundary detection algorithm

--- trunk/ImageHelperCpp/draw.cpp
+++ trunk/ImageHelperCpp/draw.cpp
@@ -9,19 +9,20 @@ namespace helper
     line(mat, graph.points.find(p1)->second * scale, graph.points.find(p2)->second * scale, scalar);
   }
 
+  void drawTriangle(cv::Mat& mat, Graph& graph, int* idx, float scale, cv::Scalar scalar)
+  {
+    drawEdge(mat, graph, idx[0], idx[1], scale, scalar);
+    drawEdge(mat, graph, idx[1], idx[2], scale, scalar);
+    drawEdge(mat, graph, idx[2], idx[0], scale, scalar);
+  }
+
   void drawGraph(cv::Mat& mat, Graph& graph, float scale, bool drawTriangles)
   {
     mat.setTo(cv::Scalar(0, 0, 0));
 
     if (drawTriangles)
-    {
       for (TriangleList::iterator tIt = graph.triangles.begin(); tIt != graph.triangles.end(); ++tIt)
-      {
-        drawEdge(mat, graph, tIt->idx[0], tIt->idx[1], scale, cv::Scalar(0, 172, 0));
-        drawEdge(mat, graph, tIt->idx[1], tIt->idx[2], scale, cv::Scalar(0, 172, 0));
-        drawEdge(mat, graph, tIt->idx[2], tIt->idx[0], scale, cv::Scalar(0, 172, 0));
-      }
-    }
+        drawTriangle(mat, graph, tIt->idx, scale, cv::Scalar(0, 172, 0));
 
     for (EdgeValueList::iterator eIt = graph.edges.begin(); eIt != graph.edges.end(); ++eIt)
       drawEdge(mat, graph, eIt->first.first, eIt->first.second, scale, cv::Scalar(0, 0, eIt->second));
@@ -38,10 +39,8 @@ namespace helper
     {
       cv::Scalar color(rand() & 155 + 100, rand() & 155 + 100, rand() & 155 + 100);
 
-      for (EdgeValueList::iterator eit = it->begin(); eit != it->end(); ++eit)
-      {
-        drawEdge(mat, graph, eit->first.first, eit->first.second, scale, color);
-      }
+      for (TriangleList::iterator tit = it->trianlges.begin(); tit != it->trianlges.end(); ++tit)
+        drawTriangle(mat, graph, tit->idx, scale, color);
     }
   }
 }
