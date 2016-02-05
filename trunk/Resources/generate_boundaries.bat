624e0009cd05609241f4d792f6a33a56e5dc1d9f
theaddy <theaddy@4c8e4b2f-98be-4288-a789-18abe27bfa1d>
2016-02-05 22:03:21 +0000

    - updated line detection algorithm

---
+++ trunk/Resources/generate_boundaries.bat
@@ -0,0 +1,7 @@
+@echo off
+
+cd ..
+call ./BUILD_DEBUG.BAT
+cd ./Resources/
+
+start /b /w "" "..\Debug\ImageHelperCpp.exe" -i ../Resources/earth_textures/boundaries.png -p ../Resources/TEMP/countries -t ..\Resources\triangle\bin\triangle.exe -D ../Resources/TEMP/dbg -d 10 -a 20 -r 0.6
