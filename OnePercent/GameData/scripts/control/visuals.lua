44070dd5d5070b0e51d0154909dcf914ea7d56c0
theaddy <46258197+bakedroll@users.noreply.github.com>
2019-02-02 07:08:11 +0000

    added class LuaObservableCallback

--- OnePercent/GameData/scripts/control/visuals.lua
+++ OnePercent/GameData/scripts/control/visuals.lua
@@ -3,3 +3,9 @@ control:on_event(defines.callback.on_initialize, function()
   visuals:bind_branch_value_to_visuals("politics", "propagated", "takeover")
 
 end)
+
+control:on_event(defines.callback.on_country_changed, function(cid)
+
+	log:debug("COUNTRY CHANGED")
+
+end)
