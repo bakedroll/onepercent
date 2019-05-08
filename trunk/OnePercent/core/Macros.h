a3766f0699318b07b10fff97dc21b6dd6f55b25c
theaddy <theaddy@4c8e4b2f-98be-4288-a789-18abe27bfa1d>
2019-05-08 10:43:20 +0000

    added underlying template to replace static_cast for enum classes

--- trunk/OnePercent/core/Macros.h
+++ trunk/OnePercent/core/Macros.h
@@ -6,3 +6,9 @@
 #define OSGG_QLOG_INFO(msg) OSGG_LOG_INFO(std::string(msg.toLocal8Bit().constData()))
 #define OSGG_QLOG_WARN(msg) OSGG_LOG_WARN(std::string(msg.toLocal8Bit().constData()))
 #define OSGG_QLOG_FATAL(msg) OSGG_LOG_FATAL(std::string(msg.toLocal8Bit().constData()))
+
+template <typename T>
+typename std::underlying_type<T>::type underlying(T t)
+{
+    return static_cast<typename std::underlying_type<T>::type>(t);
+}
