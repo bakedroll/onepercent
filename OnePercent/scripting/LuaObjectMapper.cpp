e432327f81f813ad2a23720e873e76d8ff9e3e80
baked_roll <daniel.borchart@protonmail.com>
2021-03-29 19:27:20 +0200

    implemented simple state machine

--- OnePercent/scripting/LuaObjectMapper.cpp
+++ OnePercent/scripting/LuaObjectMapper.cpp
@@ -5,7 +5,8 @@
 namespace onep
 {
   template<typename T, typename... Args>
-  std::unique_ptr<T> make_unique(Args&&... args) {
+  std::unique_ptr<T> make_unique(Args&&... args)
+  {
     return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
   }
 
--- QtOsgBridgeApplication/src/main.cpp
+++ QtOsgBridgeApplication/src/main.cpp
@@ -5,8 +5,6 @@
 #include <osgHelper/ppu/DOF.h>
 #include <osgHelper/Helper.h>
 
-#include <QApplication>
-#include <QMainWindow>
 #include <QVBoxLayout>
 #include <QPushButton>
 #include <QMouseEvent>
@@ -19,49 +17,16 @@
 #include <QtOsgBridge/VirtualOverlay.h>
 #include <QtOsgBridge/OverlayCompositor.h>
 
+#include <QtOsgBridge/QtGameApplication.h>
+#include <QtOsgBridge/EventStateBase.h>
+
 float rotation;
 double position;
 
-class EventHandler : public QObject
-{
-public:
-  EventHandler(QMainWindow* mainWindow)
-    : QObject()
-    , m_mainWindow(mainWindow)
-  {}
-
-  bool eventFilter(QObject* object, QEvent* event) override
-  {
-    if (event->type() == QEvent::Type::MouseButtonDblClick)
-    {
-      const auto mouseEvent = dynamic_cast<QMouseEvent*>(event);
-      if (mouseEvent && mouseEvent->button() == Qt::MouseButton::LeftButton)
-      {
-        if (m_mainWindow->isFullScreen())
-        {
-          m_mainWindow->showNormal();
-          return true;
-
-        }
-        m_mainWindow->showFullScreen();
-      }
-    }
-
-    return QObject::eventFilter(object, event);
-  }
-
-private:
-  QMainWindow* m_mainWindow;
-
-};
-
 template <typename TEffect>
-osg::ref_ptr<TEffect> setupPostProcessingEffect(osgHelper::ioc::InjectionContainer& container,
-                                          osgHelper::ioc::Injector& injector, const osgHelper::View::Ptr& view,
-                                          QLayout* buttonsLayout, bool enable = true)
+void setupPostProcessingEffect(const osg::ref_ptr<TEffect>& effect, const osgHelper::View::Ptr& view,
+                                                QLayout* buttonsLayout, bool enable = true)
 {
-  container.registerType<TEffect>();
-  auto effect = injector.inject<TEffect>();
   view->addPostProcessingEffect(effect, enable);
 
   auto button = new QPushButton(QString("Toggle %1").arg(QString::fromStdString(TEffect::Name)));
@@ -78,137 +43,155 @@ osg::ref_ptr<TEffect> setupPostProcessingEffect(osgHelper::ioc::InjectionContain
   });
 
   buttonsLayout->addWidget(button);
-
-  return effect;
 }
 
-void createWindow(osgHelper::ioc::InjectionContainer& container, osgHelper::ioc::Injector& injector, QtOsgBridge::QtOsgWidget::UpdateMode mode)
+class Scene : public osg::Group
 {
-  rotation = 0.0f;
-  position = 0.0;
-
-  auto geodeBox = new osg::Geode();
-  geodeBox->addDrawable(new osg::ShapeDrawable(new osg::Box()));
-
-  auto geodeSphere = new osg::Geode();
-  geodeSphere->addDrawable(new osg::ShapeDrawable(new osg::Sphere()));
-
-  auto transformSphere = new osg::PositionAttitudeTransform();
-  transformSphere->addChild(geodeSphere);
-  transformSphere->setPosition(osg::Vec3d(2.0, 0.0, 0.0));
-
-  auto transformBox = new osg::PositionAttitudeTransform();
-  transformBox->addChild(geodeBox);
-  
-  auto updateTransformation = [transformBox]()
+public:
+  Scene(osgHelper::ioc::Injector& injector)
   {
-    transformBox->setAttitude(osgHelper::getQuatFromEuler(0.3, 0.0, rotation));
-    transformBox->setPosition(osg::Vec3d(0.0, 3.0 - 5.0 * cos(position), 0.0));
-  };
+    rotation = 0.0f;
+    position = 0.0;
 
-  updateTransformation();
+    auto geodeBox = new osg::Geode();
+    geodeBox->addDrawable(new osg::ShapeDrawable(new osg::Box()));
 
-  auto group = new osg::Group();
-  group->addChild(transformBox);
-  group->addChild(transformSphere);
+    auto geodeSphere = new osg::Geode();
+    geodeSphere->addDrawable(new osg::ShapeDrawable(new osg::Sphere()));
 
-  group->getOrCreateStateSet()->setMode(GL_BLEND, osg::StateAttribute::OFF);
-  group->getOrCreateStateSet()->setMode(GL_DEPTH_TEST, osg::StateAttribute::ON);
+    auto transformSphere = new osg::PositionAttitudeTransform();
+    transformSphere->addChild(geodeSphere);
+    transformSphere->setPosition(osg::Vec3d(2.0, 0.0, 0.0));
 
-  auto qtOsgWidget = new QtOsgBridge::QtOsgWidget();
-  qtOsgWidget->setUpdateMode(mode);
-
-  auto timer = new QTimer(qtOsgWidget);
-  timer->setInterval(16);
-  timer->setSingleShot(false);
-  QObject::connect(timer, &QTimer::timeout, [updateTransformation]()
-  {
-    rotation += 0.05f;
-    position += 0.02;
-
-    if (rotation >= 2.0f * C_PI)
+    auto transformBox = new osg::PositionAttitudeTransform();
+    transformBox->addChild(geodeBox);
+    
+    auto updateTransformation = [transformBox]()
     {
-      rotation = 0.0f;
-    }
-
-    if (position >= 2.0 * C_PI)
-    {
-      position = 0.0;
-    }
+      transformBox->setAttitude(osgHelper::getQuatFromEuler(0.3, 0.0, rotation));
+      transformBox->setPosition(osg::Vec3d(0.0, 3.0 - 5.0 * cos(position), 0.0));
+    };
 
     updateTransformation();
-  });
 
-  timer->start();
+    addChild(transformBox);
+    addChild(transformSphere);
 
-  auto sceneView  = qtOsgWidget->getView();
+    getOrCreateStateSet()->setMode(GL_BLEND, osg::StateAttribute::OFF);
+    getOrCreateStateSet()->setMode(GL_DEPTH_TEST, osg::StateAttribute::ON);
 
-  auto sceneCamera = sceneView->getCamera(osgHelper::View::CameraType::Scene);
+    auto timer = new QTimer();
+    timer->setInterval(16);
+    timer->setSingleShot(false);
+    QObject::connect(timer, &QTimer::timeout, [updateTransformation]()
+    {
+      rotation += 0.05f;
+      position += 0.02;
 
-  sceneCamera->setPosition(osg::Vec3f(0, -5, 0));
-  sceneCamera->setClearColor(osg::Vec4(0.0, 0.0, 0.0, 1.0));
+      if (rotation >= 2.0f * C_PI)
+      {
+        rotation = 0.0f;
+      }
 
-  sceneView->setClampColorEnabled(true);
-  sceneView->getRootGroup()->addChild(group);
+      if (position >= 2.0 * C_PI)
+      {
+        position = 0.0;
+      }
 
-  auto screenGeode = new osg::Geode();
-  screenGeode->addDrawable(osg::createTexturedQuadGeometry(osg::Vec3(100.0, 100.0, 0.0), osg::Vec3(200.0, 0.0, 0.0),
-                                                           osg::Vec3(0.0, 100.0, 0.0)));
+      updateTransformation();
+    });
 
-  auto buttonsLayout = new QHBoxLayout();
-  auto fxaa          = setupPostProcessingEffect<osgHelper::ppu::FXAA>(container, injector, sceneView, buttonsLayout, false);
-  auto hdr           = setupPostProcessingEffect<osgHelper::ppu::HDR>(container, injector, sceneView, buttonsLayout, false);
-  auto dof           = setupPostProcessingEffect<osgHelper::ppu::DOF>(container, injector, sceneView, buttonsLayout, false);
+    timer->start();
 
-  dof->setZNear(1.0f);
-  dof->setZFar(1000.0f);
-  dof->setFocalRange(3.0f);
-  dof->setFocalLength(5.0f);
+    auto screenGeode = new osg::Geode();
+    screenGeode->addDrawable(osg::createTexturedQuadGeometry(osg::Vec3(100.0, 100.0, 0.0), osg::Vec3(200.0, 0.0, 0.0),
+                                                             osg::Vec3(0.0, 100.0, 0.0)));
+  }
+};
 
-  auto mainLayout = new QVBoxLayout();
-  mainLayout->setContentsMargins(0, 0, 0, 0);
-  mainLayout->addWidget(qtOsgWidget);
-  mainLayout->addLayout(buttonsLayout);
+class InitialState : public QtOsgBridge::EventStateBase<Scene>
+{
+public:
+  InitialState(osgHelper::ioc::Injector& injector)
+    : QtOsgBridge::EventStateBase<Scene>(injector)
+  {
+    fxaa = injector.inject<osgHelper::ppu::FXAA>();
+    dof = injector.inject<osgHelper::ppu::DOF>();
+    hdr = injector.inject<osgHelper::ppu::HDR>();
+  }
 
-  auto widget = new QWidget();
-  widget->setLayout(mainLayout);
+  void initialize(QtOsgBridge::QtOsgWidget* widget) override
+  {
+    auto sceneView = widget->getView();
+    auto sceneCamera = sceneView->getCamera(osgHelper::View::CameraType::Scene);
 
-  auto mainWindow = new QMainWindow();
-  mainWindow->setGeometry(100, 100, 1024, 768);
-  mainWindow->setCentralWidget(widget);
-  mainWindow->show();
+    sceneCamera->setPosition(osg::Vec3f(0, -5, 0));
+    sceneCamera->setClearColor(osg::Vec4(0.0, 0.0, 0.0, 1.0));
 
-  qtOsgWidget->installEventFilter(new EventHandler(mainWindow));
+    sceneView->setClampColorEnabled(true);
 
-  auto button1 = new QPushButton("Button 1");
-  auto button2 = new QPushButton("Button 2");
+    auto buttonsLayout = new QHBoxLayout();
+    setupPostProcessingEffect<osgHelper::ppu::FXAA>(fxaa, sceneView, buttonsLayout, false);
+    setupPostProcessingEffect<osgHelper::ppu::HDR>(hdr, sceneView, buttonsLayout, false);
+    setupPostProcessingEffect<osgHelper::ppu::DOF>(dof, sceneView, buttonsLayout, false);
 
-  auto overlayLayout = new QVBoxLayout();
-  overlayLayout->addWidget(button1);
-  overlayLayout->addWidget(button2);
-  overlayLayout->addStretch(1);
+    dof->setZNear(1.0f);
+    dof->setZFar(1000.0f);
+    dof->setFocalRange(3.0f);
+    dof->setFocalLength(5.0f);
 
-  auto overlay = new QtOsgBridge::VirtualOverlay();
-  overlay->setLayout(overlayLayout);
-  overlay->setGeometry(100, 100, 300, 300);
-  overlay->setStyleSheet("background-color: rgba(100, 100, 220, 50%);");
+    auto overlayLayout = new QVBoxLayout();
+    overlayLayout->addLayout(buttonsLayout);
+    overlayLayout->addStretch(1);
 
-  qtOsgWidget->getOverlayCompositor()->addVirtualOverlay(overlay);
+    auto overlay = new QtOsgBridge::VirtualOverlay();
+    overlay->setLayout(overlayLayout);
+    overlay->setGeometry(100, 100, 300, 300);
+    overlay->setStyleSheet("background-color: rgba(100, 100, 220, 50%);");
 
-  overlay->show();
+    widget->getOverlayCompositor()->addVirtualOverlay(overlay);
 
-}
+    overlay->show();
+  }
+
+  bool isLoadingState() const override
+  {
+    return false;
+  }
+
+private:
+  osg::ref_ptr<osgHelper::ppu::FXAA> fxaa;
+  osg::ref_ptr<osgHelper::ppu::DOF> dof;
+  osg::ref_ptr<osgHelper::ppu::HDR> hdr;
+};
+
+class QtOsgBridgeApplication : public QtOsgBridge::QtGameApplication
+{
+public:
+  QtOsgBridgeApplication(int& argc, char** argv)
+    : QtGameApplication(argc, argv)
+  {
+  }
+
+protected:
+  void registerComponents(osgHelper::ioc::InjectionContainer& container) override
+  {
+    registerEssentialComponents();
+
+    container.registerType<Scene>();
+    container.registerType<InitialState>();
+    container.registerType<osgHelper::ppu::DOF>();
+    container.registerType<osgHelper::ppu::FXAA>();
+    container.registerType<osgHelper::ppu::HDR>();
+  }
+};
 
 int main(int argc, char** argv)
 {
+  QtOsgBridgeApplication app(argc, argv);
+
   osgHelper::ioc::InjectionContainer container;
   osgHelper::ioc::Injector           injector(container);
 
-  container.registerSingletonType<osgHelper::ShaderFactory>();
-
-  QApplication app(argc, argv);
-
-  createWindow(container, injector, QtOsgBridge::QtOsgWidget::UpdateMode::OnTimerEvent);
-
-  return QApplication::exec();
+  return app.runGame<InitialState>();
 }
